46516f9bb8e5b4796f08230e67a79df7168e1585
paskalev2001 <2001paskalpaskalev@gmail.com>
2026-04-25 20:59:12 +0300

    Added Thymeleaf dependency & Draft Home page

---
+++ src/main/java/bg/nbu/banking_app/controller/HomeController.java
@@ -0,0 +1,17 @@
+package bg.nbu.banking_app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/index")
+    public String index() {
+        return "index";
+    }
+}
