7a2430386a97a74c00b7f481f25ecf10c4802444
paskalev2001 <2001paskalpaskalev@gmail.com>
2026-04-26 20:14:22 +0300

    Bank Account Controller

--- src/main/java/bg/nbu/banking_app/service/impl/BankAccountServiceImpl.java
+++ src/main/java/bg/nbu/banking_app/service/impl/BankAccountServiceImpl.java
@@ -6,6 +6,7 @@ import bg.nbu.banking_app.data.dto.Customers.Company.CompanyDTO;
 import bg.nbu.banking_app.data.entity.BankAccount;
 import bg.nbu.banking_app.data.entity.Company;
 import bg.nbu.banking_app.data.repository.BankAccountRepository;
+import bg.nbu.banking_app.service.BankAccountService;
 import bg.nbu.banking_app.util.MapperUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,18 +15,18 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class BankAccountServiceImpl {
+public class BankAccountServiceImpl implements BankAccountService {
     private final BankAccountRepository bankAccountRepository;
     private final MapperUtil mapperUtil;
 
-
+    @Override
     public List<BankAccountDTO> getAllBankAccounts(){
         return this.mapperUtil.mapList(
                 this.bankAccountRepository.findAll(), BankAccountDTO.class
         );
     }
 
-    //@Override is it needed?
+    @Override
     public BankAccountDTO getBankAccount(long id) {
         BankAccount bankAccount = this.bankAccountRepository
                 .findById(id)
@@ -36,6 +37,7 @@ public class BankAccountServiceImpl {
                 .map(bankAccount, BankAccountDTO.class);
     }
 
+    @Override
     public BankAccountDTO createBankAccount(BankAccountDTO bankAccount) {
         return mapperUtil.getModelMapper()
                 .map(this.bankAccountRepository
@@ -43,6 +45,7 @@ public class BankAccountServiceImpl {
                                 .map(bankAccount, BankAccount.class)), BankAccountDTO.class);
     }
 
+    @Override
     public BankAccountDTO updateBankAccount(UpdateBankAccountDTO bankAccount, long id) {
         BankAccount bankAccountCurrent = this.bankAccountRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Bank account with id " + id + " not found and cannot be updated"));
@@ -58,5 +61,6 @@ public class BankAccountServiceImpl {
     }
 
     //See if this is even needed
+    @Override
     public void deleteBankAccount(long id) { this.bankAccountRepository.deleteById(id);}
 }
