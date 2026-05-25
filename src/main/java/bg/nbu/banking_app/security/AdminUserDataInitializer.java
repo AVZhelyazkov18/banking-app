dc9c6a083d0fb0dc32a60e18275083924693e2f0
paskalev2001 <2001paskalpaskalev@gmail.com>
2026-05-25 13:38:34 +0300

    Admin User Data Initializer now creates a person that gets linked to the user as to preserve data integrity

--- src/main/java/bg/nbu/banking_app/security/AdminUserDataInitializer.java
+++ src/main/java/bg/nbu/banking_app/security/AdminUserDataInitializer.java
@@ -1,6 +1,8 @@
 package bg.nbu.banking_app.security;
 
+import bg.nbu.banking_app.data.entity.Person;
 import bg.nbu.banking_app.data.entity.User;
+import bg.nbu.banking_app.data.repository.PersonRepository;
 import bg.nbu.banking_app.data.repository.UserRepository;
 import bg.nbu.banking_app.data.enums.Role;
 import org.springframework.boot.CommandLineRunner;
@@ -12,22 +14,38 @@ public class AdminUserDataInitializer implements CommandLineRunner {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final PersonRepository personRepository;
 
     public AdminUserDataInitializer(
             UserRepository userRepository,
-            PasswordEncoder passwordEncoder
+            PasswordEncoder passwordEncoder,
+            PersonRepository personRepository
     ) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.personRepository = personRepository;
     }
 
     @Override
     public void run(String... args) {
+        Person personAdmin = personRepository.findByClientNumber("ADMIN00001")
+                .orElseGet(() -> {
+                    Person person = new Person();
+                    person.setAddress("ADMIN");
+                    person.setPin("12345678");
+                    person.setFirstName("ADMIN");
+                    person.setLastName("ADMIN");
+                    person.setClientNumber("ADMIN00001");
+
+                    return personRepository.save(person);
+                });
+
         if (!userRepository.existsByEmail("admin@test.com")) {
             User admin = new User();
             admin.setEmail("admin@test.com");
             admin.setPassword(passwordEncoder.encode("admin123"));
             admin.setRole(Role.ROLE_ADMIN);
+            admin.setCustomer(personAdmin);
 
             userRepository.save(admin);
         }
